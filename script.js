26fb0d3066c1bb55207bf54e22fb3000ae4669bc
Sky <dimiturtasev@abv.bg>
2017-05-07 18:00:06 +0300

    bugfix

--- script.js
+++ script.js
@@ -5,34 +5,17 @@ function gEBI(a) {
 var sync = 1;
 
 function abbrNum(number, decPlaces) {
-    // 2 decimal places => 100, 3 => 1000, etc
     decPlaces = Math.pow(10,decPlaces);
-
-    // Enumerate number abbreviations
     var abbrev = [ "k", "m", "b", "t" ];
-
-    // Go through the array backwards, so we do the largest first
     for (var i=abbrev.length-1; i>=0; i--) {
-
-        // Convert array index to "1000", "1000000", etc
         var size = Math.pow(10,(i+1)*3);
-
-        // If the number is bigger or equal do the abbreviation
         if(size <= number) {
-             // Here, we multiply by decPlaces, round, and then divide by decPlaces.
-             // This gives us nice rounding to a particular decimal place.
              number = Math.round(number*decPlaces/size)/decPlaces;
-
-             // Handle special case where we round up to the next abbreviation
              if((number == 1000) && (i < abbrev.length - 1)) {
                  number = 1;
                  i++;
              }
-
-             // Add the letter for the abbreviation
              number += abbrev[i];
-
-             // We are done... stop
              break;
         }
     }
@@ -302,10 +285,10 @@ function buyTreatmentPlant(a,b) {
 		gEBI("MTpump").innerHTML = treatPla[1];
 		gEBI("BTpump").innerHTML = treatPla[2];
 		gEBI("VBTpump").innerHTML = treatPla[3];
-		gEBI("STpump").innerHTML = abbrNum(treatProd[0].toFixed(0),2) + " m<sup>3</sup>";
-		gEBI("MTpump").innerHTML = abbrNum(treatProd[1].toFixed(0),2) + " m<sup>3</sup>";
-		gEBI("BTpump").innerHTML = abbrNum(treatProd[2].toFixed(0),2) + " m<sup>3</sup>";
-		gEBI("VBTpump").innerHTML = abbrNum(treatProd[3].toFixed(0),2) + " m<sup>3</sup>";
+		gEBI("STpumpProd").innerHTML = abbrNum(treatProd[0].toFixed(0),2) + " m<sup>3</sup>";
+		gEBI("MTpumpProd").innerHTML = abbrNum(treatProd[1].toFixed(0),2) + " m<sup>3</sup>";
+		gEBI("BTpumpProd").innerHTML = abbrNum(treatProd[2].toFixed(0),2) + " m<sup>3</sup>";
+		gEBI("VBTpumpProd").innerHTML = abbrNum(treatProd[3].toFixed(0),2) + " m<sup>3</sup>";
 		TW = Number(treatPla[0]) * Number(treatProd[0]) + Number(treatPla[1]) * Number(treatProd[1]) + Number(treatPla[2]) * Number(treatProd[2]) + Number(treatPla[3]) * Number(treatProd[3]);
 		gEBI("trt").innerHTML = abbrNum(TW.toFixed(2),2);
 	}
